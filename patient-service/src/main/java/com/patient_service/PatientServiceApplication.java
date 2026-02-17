f89f14176728e687c24a331633db03f0c58b48a0
Nikhil <nikhilsinghnikhilsingh242@gmail.com>
2026-02-17 14:40:08 +0530

    Testing the webhook sending

--- patient-service/src/main/java/com/patient_service/PatientServiceApplication.java
+++ patient-service/src/main/java/com/patient_service/PatientServiceApplication.java
@@ -2,7 +2,7 @@ package com.patient_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Adding this to just to test the current state
 @SpringBootApplication
 public class PatientServiceApplication {
 
